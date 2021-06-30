6eac30a98e23b5f623a713cc1bac3c7aa09dd0f3
Co1lin <i@co1in.me>
2021-06-30 19:09:56 +0800

    remove pb option to run with xvfb

--- net_utils/ap_helper.py
+++ net_utils/ap_helper.py
@@ -437,11 +437,12 @@ def batch_load_pred_data(idx, proposal_ids, batch_id, pred_corners, sem_cls_prob
 
     dimension = int(max((obj_points.max(0) - obj_points.min(0))) / voxel_size)
     dimension = max(dimension, 2)
+    print('wtf00')
     # internal voxels
-    voxel_data_internal = voxelize_mesh(mesh_data, dimension=dimension, wireframe=True, dilated_carving=True)
+    voxel_data_internal = voxelize_mesh(mesh_data, dimension=dimension, wireframe=True, dilated_carving=True, use_offscreen_pbuffer=False)
     # surface voxels
-    voxel_data_surface = voxelize_mesh(mesh_data, exact=True, dimension=dimension)
-
+    voxel_data_surface = voxelize_mesh(mesh_data, exact=True, dimension=dimension, use_offscreen_pbuffer=False)
+    print('wtf01')
     return (ii, pred_corners[batch_id, j], sem_cls_probs[batch_id, j, ii] * obj_prob[batch_id, j],
             (voxel_data_internal, voxel_data_surface))
 
@@ -455,9 +456,9 @@ def batch_load_pred_data_wo_cls(j, proposal_ids, meshes, pred_corners, batch_id,
     dimension = int(max((obj_points.max(0) - obj_points.min(0))) / voxel_size)
     dimension = max(dimension, 2)
     # internal voxels
-    voxel_data_internal = voxelize_mesh(mesh_data, dimension=dimension, wireframe=True, dilated_carving=True)
+    voxel_data_internal = voxelize_mesh(mesh_data, dimension=dimension, wireframe=True, dilated_carving=True, use_offscreen_pbuffer=False)
     # surface voxels
-    voxel_data_surface = voxelize_mesh(mesh_data, exact=True, dimension=dimension)
+    voxel_data_surface = voxelize_mesh(mesh_data, exact=True, dimension=dimension, use_offscreen_pbuffer=False)
 
     return (pred_sem_cls[batch_id, j], pred_corners[batch_id, j], obj_prob[batch_id, j],
             (voxel_data_internal, voxel_data_surface))
@@ -472,7 +473,7 @@ def batch_load_gt_data(j, meshes, gt_corners, sem_cls_label, batch_id, voxel_siz
     dimension = int(max((obj_points.max(0) - obj_points.min(0))) / voxel_size)
     dimension = max(dimension, 2)
     # internal voxels
-    voxel_data_internal = voxelize_mesh(mesh_data, dimension=dimension, wireframe=True, dilated_carving=True)
+    voxel_data_internal = voxelize_mesh(mesh_data, dimension=dimension, wireframe=True, dilated_carving=True, use_offscreen_pbuffer=False)
     # surface voxels
-    voxel_data_surface = voxelize_mesh(mesh_data, exact=True, dimension=dimension)
+    voxel_data_surface = voxelize_mesh(mesh_data, exact=True, dimension=dimension, use_offscreen_pbuffer=False)
     return (sem_cls_label[batch_id, j], gt_corners[batch_id, j], (voxel_data_internal, voxel_data_surface))
